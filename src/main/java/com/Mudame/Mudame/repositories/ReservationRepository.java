f958eea854e9d8171eb685d643c93b5829d12008
Marulanda921 <Alejandromarulanda702@gmail.com>
2024-05-02 11:16:18 -0500

    Cambiando importacion de clases en las interfaces

--- src/main/java/com/Mudame/Mudame/repositories/ReservationRepository.java
+++ src/main/java/com/Mudame/Mudame/repositories/ReservationRepository.java
@@ -1,8 +1,9 @@
 package com.Mudame.Mudame.repositories;
 
+import com.Mudame.Mudame.Entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservationRepository {
-    
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 }
--- src/main/java/com/Mudame/Mudame/repositories/UserRepository.java
+++ src/main/java/com/Mudame/Mudame/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.Mudame.Mudame.repositories;
 
+import com.Mudame.Mudame.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.Mudame.Mudame.entity.User;
+
 
 @Repository
 //carlox "no estoy seguro si se debe poner integer aqui"
