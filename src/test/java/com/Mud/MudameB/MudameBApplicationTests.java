54443e20270f1324a3c6fd7488b28baef678c736
Alejandro <alejandromarulanda702@gmail.com>
2024-05-06 18:56:21 -0500

    01

---
+++ src/test/java/com/Mud/MudameB/MudameBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mud.MudameB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudameBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
