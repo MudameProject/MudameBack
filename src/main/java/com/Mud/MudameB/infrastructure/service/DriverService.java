1951f95ff8c91d61827defeb9f0c13fa2da7813e
David osorio <daalosx@gmail.com>
2024-05-09 07:47:47 -0500

    guardando validadores de errores

--- src/main/java/com/Mud/MudameB/infrastructure/service/DriverService.java
+++ src/main/java/com/Mud/MudameB/infrastructure/service/DriverService.java
@@ -2,14 +2,29 @@ package com.Mud.MudameB.infrastructure.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
 
+import com.Mud.MudameB.Domain.Entity.DriverEntity;
+import com.Mud.MudameB.Domain.repositories.DriverRepository;
 import com.Mud.MudameB.Utils.enums.SortType;
+import com.Mud.MudameB.Utils.exceptions.BadRequestException;
+import com.Mud.MudameB.Utils.messages.ErrorMessages;
 import com.Mud.MudameB.api.dto.request.DriverReq;
 import com.Mud.MudameB.api.dto.response.DriverResp;
 import com.Mud.MudameB.infrastructure.abstract_services.IDriverService;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
+@Service
+@Transactional
+@AllArgsConstructor
 public class DriverService implements IDriverService {
+
+  @Autowired
+  private final DriverRepository driverRepository;
 
   @Override
   public DriverResp create(DriverReq request) {
@@ -47,4 +62,8 @@ public class DriverService implements IDriverService {
     throw new UnsupportedOperationException("Unimplemented method 'search'");
   }
 
+  private DriverEntity find(Long id) {
+    return this.driverRepository.findById(id)
+        .orElseThrow(() -> new BadRequestException(ErrorMessages.idNotFound("Conductor")));
+  }
 }
