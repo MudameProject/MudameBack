0a577a1dddcaf7f1400011f3a3d2500834ea3083
nicoluna319 <nicolaslunaromero@gmail.com>
2024-05-16 09:36:21 -0500

    primer metodo getAll

--- src/main/java/com/Mud/MudameB/api/controllers/TruckController.java
+++ src/main/java/com/Mud/MudameB/api/controllers/TruckController.java
@@ -1,14 +1,18 @@
 package com.Mud.MudameB.api.controllers;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
+import com.Mud.MudameB.Utils.enums.Capacity;
 import com.Mud.MudameB.api.dto.response.TruckResp;
 import com.Mud.MudameB.infrastructure.service.TruckService;
 
@@ -24,8 +28,10 @@ public class TruckController {
     private final TruckService truckService;
 
     @GetMapping
-    public ResponseEntity<Page<TruckResp>> getAll(@Validated @RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size){
-        return ResponseEntity.ok(this.truckService.getAll(page, size));
+    public ResponseEntity<Page<TruckResp>> getAll(@Validated @RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestHeader(required = false) Capacity CAPACITY_SEARCH) {
+        if (Objects.isNull(CAPACITY_SEARCH))
+            CAPACITY_SEARCH = Capacity.NONE;
+        return ResponseEntity.ok(this.truckService.getAll(page - 1 , size, CAPACITY_SEARCH));
     };
 
 
