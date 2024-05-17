801e5ba4dfe74b4d53758dbb297ffa5161de261d
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-05-17 10:47:33 -0500

    swagger eliminado

--- src/main/java/com/Mud/MudameB/api/controllers/AuthController.java
+++ src/main/java/com/Mud/MudameB/api/controllers/AuthController.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import com.Mud.MudameB.api.dto.request.LoginReq;
 import com.Mud.MudameB.api.dto.request.RegisterReq;
