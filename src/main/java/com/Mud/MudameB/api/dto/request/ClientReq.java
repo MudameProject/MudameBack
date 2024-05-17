6121de7fac2e4bd6e4e52e53c921fa734c746968
Marulanda921 <Alejandromarulanda702@gmail.com>
2024-05-17 12:25:06 -0500

    Verificando la creacion correcta del jwt, se genera correctamente

--- src/main/java/com/Mud/MudameB/api/dto/request/ClientReq.java
+++ src/main/java/com/Mud/MudameB/api/dto/request/ClientReq.java
@@ -21,8 +21,8 @@ public class ClientReq {
     @Email(message = "el email no es valido")
     @Size(min = 5, max = 100, message = "el email debe tener entre 5 y 100 caracteres")
     private String email;
-    @Size(min = 10, max = 20, message = "el telefono debe tener entre 10 y 20 caracteres")
-    private Integer phoneNumber;
+    @Size(min = 10, max = 20, message = "El error excede")
+    private int phoneNumber;
     private String address;
     @NotNull(message = "el código postal es requerido")
     private Integer zipCode;
